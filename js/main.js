d6c6c5a456ded78131748b2fdcbdeaa86b8063b1
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-08 11:24:23 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,24 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+
+  function countUp(){
+    const d = new Date(Date.now() - startTime);
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+    const ms = d.getMilliseconds();
+    timer.textContent = `${String(m).padStart(2, '0')}:${String(s).padStart(2,
+       '0')}.${String(ms).padStart(3, '0')}`;
+
+    setTimeout(() =>{
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', ()=> {
+    startTime = Date.now();
+    countUp();
+  });
 }
